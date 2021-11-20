bbec915282cf10f3cdd995319bbbe5e61b56fe38
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-11-20 15:54:57 -0800

    refactored Utilities class

--- light-client/src/main/java/tech/pegasys/teku/lightclient/server/Utilities.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/server/Utilities.java
@@ -1,7 +1,28 @@
 package tech.pegasys.teku.lightclient.server;
 
-public class Utilities {
+import java.lang.reflect.Array;
 
-	//Code to be added
+import tech.pegasys.teku.ssz.cache.ArrayIntCache;
+
+public class Utilities {
 	
+	private ArrayIntCache<Array>[] bits;
+	public ArrayIntCache<Array>[] getBits() {
+		return bits;
+	}
+	public void setBits(ArrayIntCache<Array>[] bits) {
+		this.bits = bits;
+	}
+	
+	public int sumBits() {
+		int sum = 0;
+		for (int bit: bits) {
+			if (bit) {
+				sum++;
+			}
+		}
+		return sum;
+	}
+	
+
 }
--- light-client/src/main/java/tech/pegasys/teku/lightclient/utilities/Utilities.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/utilities/Utilities.java
@@ -6,23 +6,7 @@ import tech.pegasys.teku.ssz.cache.ArrayIntCache;
 
 public class Utilities {
 	
-	private ArrayIntCache<Array>[] bits;
-	public ArrayIntCache<Array>[] getBits() {
-		return bits;
-	}
-	public void setBits(ArrayIntCache<Array>[] bits) {
-		this.bits = bits;
-	}
-	
-	public int sumBits() {
-		int sum = 0;
-		for (int bit: bits) {
-			if (bit) {
-				sum++;
-			}
-		}
-		return sum;
-	}
+//Code to be added
 	
 
 }
