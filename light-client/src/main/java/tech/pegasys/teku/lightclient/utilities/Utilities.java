ae0b63bb9d9c865b2e9c2eed6cda2c9a02e7efc8
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-11-20 15:51:25 -0800

    Started work on Utilities

--- light-client/src/main/java/tech/pegasys/teku/lightclient/utilities/Utilities.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/utilities/Utilities.java
@@ -1,7 +1,28 @@
 package tech.pegasys.teku.lightclient.utilities;
 
+import java.lang.reflect.Array;
+
+import tech.pegasys.teku.ssz.cache.ArrayIntCache;
+
 public class Utilities {
 	
-	//Code to be added
+	private ArrayIntCache<Array>[] bits;
+	public ArrayIntCache<Array>[] getBits() {
+		return bits;
+	}
+	public void setBits(ArrayIntCache<Array>[] bits) {
+		this.bits = bits;
+	}
+	
+	public int sumBits() {
+		int sum = 0;
+		for (int bit: bits) {
+			if (bit) {
+				sum++;
+			}
+		}
+		return sum;
+	}
+	
 
 }
