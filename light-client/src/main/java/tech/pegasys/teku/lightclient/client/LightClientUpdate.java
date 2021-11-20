6f566947ce4ac52516fbf8bfc1dbeaf54b11b93b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-11-20 11:41:37 -0800

    Lodestar Build Reference.txt removed no longer relevant

--- light-client/src/main/java/tech/pegasys/teku/lightclient/client/LightClientUpdate.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/client/LightClientUpdate.java
@@ -1,6 +1,7 @@
 package tech.pegasys.teku.lightclient.client;
 
 import tech.pegasys.teku.spec.datastructures.blocks.BeaconBlockHeader;
+import tech.pegasys.teku.ssz.collections.SszByteVector;
 
 public class LightClientUpdate {
 	
@@ -13,22 +14,35 @@ public class LightClientUpdate {
 		this.header = header;
 	}
 	
+	
 	/** Next sync committee corresponding to the header */
-	  private SyncCommittee nextSyncCommittee;	
-	  public SyncCommittee getNextSyncCommittee() {
+	private SyncCommittee nextSyncCommittee;	
+	public SyncCommittee getNextSyncCommittee() {
 	    return nextSyncCommittee;
-	  }
-	  public void setNextSyncCommittee(SyncCommittee nextSyncCommittee) {
+	}
+	public void setNextSyncCommittee(SyncCommittee nextSyncCommittee) {
 	    this.nextSyncCommittee = nextSyncCommittee;
-	  }
-
+	}
 //	  nextSyncCommitteeBranch: Vector<Bytes32>;
-	  /** Finality proof for the update header */
-//	  finalityHeader: phase0.BeaconBlockHeader;
+	  
+	  
+	/** Finality proof for the update header */
+	private BeaconBlockHeader finalityHeader;
+	public BeaconBlockHeader getFinalityHeader() {
+		return finalityHeader;
+	}
+	public void setFinalityHeader(BeaconBlockHeader finalityHeader) {
+			this.finalityHeader = finalityHeader;
+	}	
+	private SszByteVector sample;
 //	  finalityBranch: Vector<Bytes32>;
+		
+		
 	  /** Sync committee aggregate signature */
 //	  syncCommitteeBits: BitVector;
 //	  syncCommitteeSignature: BLSSignature;
+		
+		
 	  /** Fork version for the aggregate signature */
 //	  forkVersion: Version;
 
