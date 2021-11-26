5a00f6de43e614d1f0bb8d082e49e049f7c3fb5b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-11-25 17:39:08 -0800

    ArrayLike generic array completed

--- light-client/src/main/java/tech/pegasys/teku/lightclient/server/ArrayLike.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/server/ArrayLike.java
@@ -1,9 +1,28 @@
-package tech.pegasys.teku.lightclient.server;
+package tech.pegasys.teku.lightclient.server;;
 
-public interface ArrayLike<T> {
-	
-//	  [n: number]: T;
-//	  readonly length: number;
-//	  [Symbol.iterator](): Iterator<T>;
+public class ArrayLike<T>
+{
+    private final Object[] arr;
+    public final int number;
+ 
+    // constructor
+    public ArrayLike(int number)
+    {
+        // Creates a new object array of the specified length
+        arr = new Object[number];
+        this.number = number;
+    }
+ 
+    // Method to get object present at index `i` in the array
+    T get(int i) {
+        @SuppressWarnings("unchecked")
+        final T e = (T)arr[i];
+        return e;
+    }
+ 
+    // Method to set a value `e` at index `i` in the array
+    void set(int i, T e) {
+        arr[i] = e;
+    }
 
 }
--- light-client/src/main/java/tech/pegasys/teku/lightclient/server/Utilities.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/server/Utilities.java
@@ -4,15 +4,6 @@ package tech.pegasys.teku.lightclient.server;
 
 public class Utilities {
 	
-//	private ArrayIntCache<Boolean> bits;
-//	public ArrayIntCache<Boolean> getBits() {
-//		return bits;
-//	}
-//	public void setBits(ArrayIntCache<Boolean> bits) {
-//		this.bits = bits;
-//	}
-	
-	
 	public int sumBits(ArrayLike<Boolean> bits) {
 		int sum = 0;
 		for (boolean bit: bits) {
