0257a49e6cbc6fc348672104e4c45e1bbabb0743
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-12-25 12:44:01 -0600

    ToBlockHeader class completed

--- light-client/src/main/java/tech/pegasys/teku/lightclient/utilities/ToBlockHeader.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/utilities/ToBlockHeader.java
@@ -3,13 +3,28 @@ package tech.pegasys.teku.lightclient.utilities;
 import tech.pegasys.teku.spec.datastructures.blocks.BeaconBlockHeader;
 import tech.pegasys.teku.spec.datastructures.blocks.NodeSlot;
 import tech.pegasys.teku.spec.datastructures.blocks.SlotAndBlockRoot;
+import tech.pegasys.teku.spec.datastructures.blocks.blockbody.versions.altair.BeaconBlockBodyAltair;
 
 public class ToBlockHeader extends BeaconBlockHeader {
-    private NodeSlot block;
-    public NodeSlot toBlockHeader () {
-        //"ProposerIndex" might not be necessary here, but to be added if need be.
-        SlotAndBlockRoot slotAndBlockRoot;
+
+    public ToBlockHeader(NodeSlot block, SlotAndBlockRoot slotandBlockRoot, BeaconBlockBodyAltair bodyRoot) {
+        this.block = block;
+        this.slotandBlockRoot = slotandBlockRoot;
+        this.bodyRoot = bodyRoot;
+    }
+
+    public final NodeSlot block;
+    public NodeSlot block () {
         return block;
     }
+    public final SlotAndBlockRoot slotandBlockRoot;
+    public SlotAndBlockRoot slotandBlockRoot () {
+        return slotandBlockRoot;
+    }
+    public final BeaconBlockBodyAltair bodyRoot;
+    public BeaconBlockBodyAltair bodyRoot () {
+        return bodyRoot;
+    }
+
 
 }
