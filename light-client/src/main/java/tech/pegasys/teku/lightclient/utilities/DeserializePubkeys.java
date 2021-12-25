79c4c18abeea3e55ff37b3f42b68a31d5c3b184b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-12-25 13:53:12 -0600

    Functions to be added

--- light-client/src/main/java/tech/pegasys/teku/lightclient/utilities/DeserializePubkeys.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/utilities/DeserializePubkeys.java
@@ -1,6 +1,11 @@
 package tech.pegasys.teku.lightclient.utilities;
 
 import tech.pegasys.teku.bls.impl.PublicKey;
+import tech.pegasys.teku.lightclient.client.LightClientUpdate;
 
-public interface DeserializePubkeys extends PublicKey [] {
+public interface DeserializePubkeys extends PublicKey {
+
+    //nextSyncCommittee function to be added
+    //pubKeys function to be added
+
 }
